3c495e3f395310bfb9517435f076cc67ba6259b6
Bryan <bryan@bryanle.io>
2017-08-06 13:34:48 -0400

    Set up first test with supertest

---
+++ test/app_test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const request = require('supertest');// We will download npm package 'supertest' to replicate HTTP requests 
+const app = require('../app'); // Requiring express application
+
+// We would like to test to ensure that the app listens for incoming requests of type GET on /api and assert response of the object 
+describe('The express app', () => {
+    it('handles a GET request to /api', (done) => { // 'done' callback required for asynchronous task; HTTP requests are async in nature, most likely will always need 'done'
+        request(app)
+            .get('/api')
+            .end((err, response) => {
+                console.log(response);
+            });
+    });
+});
