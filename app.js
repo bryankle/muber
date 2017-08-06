cd35e33b099f1e0308e6ce0c19e4fcc6b6e3ab23
Bryan <bryan@bryanle.io>
2017-08-06 14:57:07 -0400

    Implemented body-parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express'); // Express is used to simplify HTTP requests through Node; everything that can be done on Express can be done on Node
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const app = express();
+// ALWAYS place app.use above the routes call
+app.use(bodyParser.json()); // Why are we referencing the JSON and its method? -> this assumes anything coming in is JSON and parse whatever comes in
 
 // Router
 // Watch for incoming requests of method GET
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
     // App listens on '/api' and executes callback when GET is requested. Takes 2 arguments, req and res object 
-    app.get('/api', DriversController.greeting)
+    app.get('/api', DriversController.greeting);
 
+    app.post('/api/drivers', DriversController.create);
 };
