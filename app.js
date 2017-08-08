aa6029ce79709a8513b33514de0e679f0a514ed6
Bryan <bryan@bryanle.io>
2017-08-07 21:47:04 -0400

    Implementing middleware test - error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes = require('./routes/routes');
 const app = express();
 
 mongoose.Promise = global.Promise; // Depracation notice fix
-if (process.env.NODE_ENV !== 'test') { // If running a development environment, run normal database to not drop collection after each test
+if (process.env.NODE_ENV !== 'test') { // If running a development environment, run normal database to not drop collection while running
     mongoose.connect('mongodb://localhost/muber'); // Connects MongoDB to Mongoose; sometimes error or success message displays here   
 }
 
@@ -16,5 +16,13 @@ app.use(bodyParser.json()); // Why are we referencing the JSON and its method? -
 // Watch for incoming requests of method GET
 // to the route http://localhost:3050/api 
 routes(app);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+})
+// err - throws an error if previous middleware produced an error
+// req - incoming request object
+// res - outgoing response object
+// next - not like the others in any way; next is a function, forcibly called to execute next middleware
 
 module.exports = app;
