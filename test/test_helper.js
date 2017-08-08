c2c6a6aab35a2227ee4717019c9f7c94e72e121e
Bryan <bryan@bryanle.io>
2017-08-07 20:45:30 -0400

    Check if test environment before connecting to muber_test database

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,4 +2,9 @@ const mongoose = require('mongoose');
 
 before(done => {
     mongoose.connect('mongodb://localhost/muber_test');
+    mongoose.connection
+        .once('open', () => done())
+        .on('error', err => {
+            console.log('Warning', error);
+        });
 });
