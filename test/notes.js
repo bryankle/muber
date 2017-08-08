d229941a18a23849c8186510371a269137140468
Bryan <bryan@bryanle.io>
2017-08-07 21:37:38 -0400

    Note regarding initiating test vs dev environment

--- test/notes.js
+++ test/notes.js
@@ -2,4 +2,7 @@
 
 No need for test helper unless working with Mongoose
 
+How does the application determine when we run a development environment?
+    - Test environment only runs when user initates via npm run test, otherwise normal npm start of application will run as usual
+
 */
