78690196b5d81c6e9ed32f82604694213c208fe3
Bryan <bryan@bryanle.io>
2017-08-07 21:57:12 -0400

    Error message appears as object through res object in postman

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json()); // Why are we referencing the JSON and its method? -
 routes(app);
 
 app.use((err, req, res, next) => {
-    console.log(err);
+    res.send({ error: err.message });
 })
 // err - throws an error if previous middleware produced an error
 // req - incoming request object
