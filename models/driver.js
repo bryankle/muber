ab85e84ca5c2dd1f432e622a80f2d9fe531ae352
Bryan <bryan@bryanle.io>
2017-08-08 00:10:54 -0400

    Added PointSchema into same file due to small size

--- models/driver.js
+++ models/driver.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// Adding PointSchema into driver.js because it is too small to separate into a new file
+const PointSchema = new Schema({ // For GeoJSON
+    type: { type: String, default: 'Point' },
+    coordinates: { type: [Number], index: '2dsphere' }
+})
 
 const DriverSchema = new Schema({
     email: {
@@ -9,7 +14,8 @@ const DriverSchema = new Schema({
     driving: {
         type: Boolean,
         default: false // Driver will default to not driving upon starting application
-    }
+    },
+    geometry: PointSchema
 });
 
 const Driver = mongoose.model('driver', DriverSchema);
