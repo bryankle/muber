6564a141903ba3953ab0285b8bd026216b607be2
Bryan <bryan@bryanle.io>
2017-08-07 20:49:03 -0400

    Added drop driver collection for testing environment

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-before(done => {
+before(done => { // Checks if test environment is active; note that conditional to determine database being run is not in app.js; why?
     mongoose.connect('mongodb://localhost/muber_test');
     mongoose.connection
         .once('open', () => done())
         .on('error', err => {
             console.log('Warning', error);
         });
+
+    beforeEach(done => {
+        const { drivers } = mongoose.connection.collections;
+        drivers.drop()
+            .then(() => done()) 
+            .catch(() => done()); // Very first time database runs error thrown to drop non existent collection
+    })
 });
